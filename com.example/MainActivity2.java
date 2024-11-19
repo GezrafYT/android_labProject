bffb34198d50efc1b3e44ef9b80fb6b3a13229a6
Gezraf <59087430+GezrafYT@users.noreply.github.com>
2024-11-19 11:07:06 +0200

    Add files via upload

---
+++ com.example/MainActivity2.java
@@ -0,0 +1,29 @@
+package com.example.lab_guyshitrit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.Button;
+
+import java.util.Random;
+
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two);
+
+        final View layout = findViewById(R.id.mainLayout2);
+
+    }
+
+
+
+
+}
